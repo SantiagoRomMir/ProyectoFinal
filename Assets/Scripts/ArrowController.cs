1560bdfddb8c6806fe537b83baad2bfebee37cbc
RaulGarCan <10303@cifpceuta.es>
2025-05-06 06:40:17 +0200

    Arreglo Merge

---
+++ Assets/Scripts/ArrowController.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI.Table;
+
+public class ArrowController : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+    public int damage;
+    public GameObject finalPosTest;
+    private Vector2 finalPos;
+    private Vector2 direction;
+    private float hitTargetTime;
+    private bool targetHit;
+    private Vector2 targetPos;
+    public string hitTag;
+    private void Awake()
+    {
+        hitTag = "Player";
+
+        finalPos = transform.parent.GetComponent<ArcherController>().targetPos + new Vector2(0, 0);
+        targetPos = finalPos;
+        direction = finalPos - (Vector2)transform.position;
+        hitTargetTime = Time.time;
+        targetHit = false;
+    }
+    private void Start()
+    {
+        float rot_z = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, -rot_z);
+    }
+    void Update()
+    {
+        Vector2 pos = transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, finalPos, Time.deltaTime*speed);
+        finalPos += direction * 1.1f;
+
+        if (Mathf.Abs(transform.position.x - targetPos.x) < 0.5f && !targetHit)
+        {
+            Debug.Log("HitTarget");
+            hitTargetTime = Time.time;
+            targetHit = true;
+        }
+        if (pos.Equals(transform.position) || Time.time >= hitTargetTime + lifeTime && targetHit)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision != null && collision.CompareTag(hitTag))
+        {
+            switch (hitTag)
+            {
+                case "Player":
+                    collision.GetComponent<PlayerController>().HurtPlayer(damage, transform.position, false);
+                    Destroy(gameObject);
+                    break;
+                case "Enemy":
+                    transform.parent.GetComponent<EnemyController>().HurtEnemy(damage);
+                    Destroy(gameObject);
+                    break;
+            }
+        }
+        if (collision != null && collision.gameObject.layer == 6)
+        {
+            Debug.Log("GroundHit: "+collision.gameObject.name);
+            Destroy(gameObject);
+        }
+    }
+    public void DeflectArrow()
+    {
+        hitTag = "Enemy";
+
+        targetPos = transform.parent.position;
+        finalPos = transform.parent.position;
+        direction = finalPos - (Vector2)transform.position;
+        hitTargetTime = Time.time;
+        targetHit = false;
+
+        float rot_z = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, -rot_z);
+
+        Debug.Log("Deflecting Arrow new TargetPos: " + finalPos);
+    }
+}
---
+++ Assets/Scripts/MeleeController.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class MeleeController : MonoBehaviour
+{
+    public bool chase;
+    public GameObject weapon;
+    public int damage;
+    public float attackCooldown;
+    public float attackRange;
+
+    private void Awake()
+    {
+        chase = false;
+        weapon.GetComponent<WeaponController>().damage = damage;
+    }
+
+    private void Start()
+    {
+        StartCoroutine("Attack");
+    }
+    private void Update()
+    {
+        if (chase)
+        {
+            Chase();
+        }
+        FlipWeapon();
+    }
+    private void FlipWeapon()
+    {
+        if (GetComponent<EnemyController>().direction == -1)
+        {
+            weapon.transform.localPosition = new Vector2(Mathf.Abs(weapon.transform.localPosition.x) * -1, weapon.transform.localPosition.y);
+        }
+        else if (GetComponent<EnemyController>().direction == 1)
+        {
+            weapon.transform.localPosition = new Vector2(Mathf.Abs(weapon.transform.localPosition.x), weapon.transform.localPosition.y);
+        }
+    }
+    private void Chase()
+    {
+        if (Vector2.Distance(transform.position, GetComponent<EnemyController>().player.transform.position) < 1.5f)
+        {
+            GetComponent<EnemyController>().stop = 0;
+            if (transform.position.x - GetComponent<EnemyController>().player.transform.position.x < 0)
+            {
+                GetComponent<EnemyController>().direction = 1;
+            }
+            else
+            {
+                GetComponent<EnemyController>().direction = -1;
+            }
+        }
+        else
+        {
+            GetComponent<EnemyController>().stop = 1;
+        }
+        if (transform.position.x - GetComponent<EnemyController>().player.transform.position.x < 0)
+        {
+            GetComponent<EnemyController>().direction = 1;
+        }
+        else
+        {
+            GetComponent<EnemyController>().direction = -1;
+        }
+        GetComponent<EnemyController>().phisics.velocity = new Vector2(GetComponent<EnemyController>().direction * GetComponent<EnemyController>().speedEenemy /* PlayerPrefs.GetFloat("dificultadV")*/ * GetComponent<EnemyController>().stop, GetComponent<EnemyController>().phisics.velocity.y);
+
+
+    }
+    IEnumerator AttackAnim()
+    {
+        GetComponent<EnemyController>().stop = 0;
+        weapon.GetComponent<Collider2D>().enabled = true;
+        weapon.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        GetComponent<EnemyController>().stop = 1;
+        weapon.GetComponent<Collider2D>().enabled = false;
+        weapon.SetActive(false);
+    }
+    IEnumerator Attack()
+    {
+        while (true)
+        {
+            if (Vector2.Distance(transform.position, GetComponent<EnemyController>().player.transform.position) <= attackRange)
+            {
+                StartCoroutine("AttackAnim");
+            }
+            yield return new WaitForSeconds(attackCooldown);
+        }
+    }
+    public void ReturnPatrol()
+    {
+        if (Vector2.Distance(GetComponent<EnemyController>().posEnd.position, transform.position) < Vector2.Distance(GetComponent<EnemyController>().posInitial, transform.position))
+        {
+            GetComponent<EnemyController>().movetoEnd = false;
+            if (transform.position.x < GetComponent<EnemyController>().posInitial.x)
+            {
+                GetComponent<EnemyController>().direction = 1;
+            }
+            else
+            {
+                GetComponent<EnemyController>().direction = -1;
+            }
+        }
+        else
+        {
+            GetComponent<EnemyController>().movetoEnd = true;
+            if (transform.position.x < GetComponent<EnemyController>().posEnd.localPosition.x)
+            {
+                GetComponent<EnemyController>().direction = 1;
+            }
+            else
+            {
+                GetComponent<EnemyController>().direction = -1;
+            }
+        }
+    }
+}
