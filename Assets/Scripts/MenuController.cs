68baf5a2e4aa619570f5ea41a234292b538db96e
RaulGC <raulius2003@gmail.com>
2025-05-19 19:14:05 +0200

    Escena Boss y Creditos

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -5,9 +5,50 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    public void Play()
+    private IEnumerator Play()
     {
+        yield return new WaitForSeconds(1f);
         PlayerPrefs.SetInt("clearPersistenceData", 1);
         SceneManager.LoadScene("Inicio");
+    }
+    public void Jugar()
+    {
+        StartCoroutine("Play");
+    }
+    public IEnumerator Controls()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("Controls");
+    }
+    public void Controles()
+    {
+        StartCoroutine("Controls");
+    }
+    public IEnumerator Credits()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("Credits");
+    }
+    public void Creditos()
+    {
+        StartCoroutine("Credits");
+    }
+    public void Salir()
+    {
+        StartCoroutine("exit");
+    }
+    private IEnumerator exit()
+    {
+        yield return new WaitForSeconds(1f);
+        Application.Quit();
+    }
+    public void Menu()
+    {
+        StartCoroutine("MainMenu");
+    }
+    private IEnumerator MainMenu()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("MainMenu");
     }
 }
