c69cf52e9727ed9bba7bac535547cd9e028f468f
RaulGC <raulius2003@gmail.com>
2025-05-19 20:15:57 +0200

    Añadido HUD Actualizado y BossFight

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -32,10 +32,10 @@ public class PlayerController : MonoBehaviour
     public float jumpTime;
     private float jumpTimeCounter;
 
-    public int maxHp;
-    private int hp;
-    public int maxRon;
-    private int ron;
+    public float maxHp;
+    private float hp;
+    public float maxRon;
+    private float ron;
 
     private bool isGrounded;
     private bool isJumping;
@@ -71,7 +71,7 @@ public class PlayerController : MonoBehaviour
     public float parryDuration;
     private float lastTimeParry;
     public float perfectParryTimeWindow;
-    public int internalDamage;
+    public float internalDamage;
     private float lastTimeHurt;
     private bool isHealingInternalDamage;
     public float healInternalDamageDelay;
@@ -574,7 +574,7 @@ public class PlayerController : MonoBehaviour
     }
     public void Heal()
     {
-        if (isCrouching || !canMove || !isGrounded || isResting)
+        if (isCrouching || !canMove || !isGrounded || isResting || !(ron > 0) || hp == maxHp)
         {
             return;
         }
@@ -834,6 +834,7 @@ public class PlayerController : MonoBehaviour
         float forwardDir = GetFacingDirection();
         bulletPrefab.GetComponent<BulletController>().direction = forwardDir;
         Instantiate(bulletPrefab, new Vector2(transform.position.x + forwardDir, transform.position.y+0.45f), Quaternion.identity);
+        hudControl.ActiveGunPowder();
     }
     private int GetFacingDirection()
     {
@@ -852,6 +853,7 @@ public class PlayerController : MonoBehaviour
         yield return new WaitForSeconds(reloadTime);
         canShoot = true;
         isReloading = false;
+        hudControl.ActiveGunPowder();
     }
     private void CheckAttackCombo()
     {
@@ -940,6 +942,8 @@ public class PlayerController : MonoBehaviour
             StartCoroutine("Dead");
         }
         internalDamage = 0;
+        hudControl.UpdatePlayerLife(hp / maxHp);
+        hudControl.UpdateInternalDamage(hp / maxHp);
         lastTimeHurt = Time.time;
         StartCoroutine("HitInvulnerable");
     }
@@ -1039,7 +1043,7 @@ public class PlayerController : MonoBehaviour
         internalDamage += addInternalDamage;
         lastTimeHurt = Time.time;
         isHealingInternalDamage = false;
-        hudControl.UpdatePlayerLife(hp-internalDamage/maxHp);
+        hudControl.UpdatePlayerLife((hp - internalDamage) / maxHp);
     }
     IEnumerator HealInternalDamage()
     {
@@ -1048,7 +1052,7 @@ public class PlayerController : MonoBehaviour
         while (internalDamage > 0)
         {
             internalDamage--;
-            hudControl.UpdatePlayerLife(hp-internalDamage/maxHp);
+            hudControl.UpdatePlayerLife((hp - internalDamage) / maxHp);
             yield return new WaitForSeconds(0.1f);
         }
         Debug.Log("Player Finished HealingInternalDamage: " + internalDamage);
