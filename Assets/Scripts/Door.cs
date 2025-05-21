597a568d359bc0db2650071bf58f34429ad6016c
SantiagoRome <9501@cifpCeuta.es>
2025-05-21 16:44:14 +0200

    conexion ciudad-interiores

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -9,22 +9,32 @@ public class Door : MonoBehaviour
 {
     public String Scene;
     private SceneSelector sceneSelector;
+    private Animator anim;
     private void Awake()
     {
+        anim = GetComponent<Animator>();
         sceneSelector = GetComponent<SceneSelector>();
         Scene = sceneSelector.GetSelectedScene();
     }
     // Start is called before the first frame update    
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Player")){
-            
-            if(Input.GetKey(KeyCode.UpArrow)){
-                PlayerPrefs.SetString("accion","puerta");
-                PlayerPrefs.SetString("Door",gameObject.name);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                PlayerPrefs.SetString("accion", "puerta");
+                PlayerPrefs.SetString("Door", gameObject.name);
                 collision.gameObject.GetComponent<PlayerController>().SavePersistenceData();
-                SceneManager.LoadScene(Scene);
+                StartCoroutine("Abrir");
             }
         }
+    }
+    private IEnumerator Abrir()
+    {
+        anim.Play("Abrir");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(Scene);
     }
 }
