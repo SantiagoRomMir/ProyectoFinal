a99004145353d1c2131672f63c12861cfc13577d
RaulGC <raulius2003@gmail.com>
2025-05-26 19:43:49 +0200

    Inventario HUD Terminado

--- Assets/Scripts/ConsumableController.cs
+++ Assets/Scripts/ConsumableController.cs
@@ -11,6 +11,7 @@ public class ConsumableController : MonoBehaviour
     private float spawnTime;
     private float speed;
     public RuntimeAnimatorController moneyAnimator;
+    public Sprite spriteDatil, spriteHierbabuena, spritePera;
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -34,6 +35,7 @@ public class ConsumableController : MonoBehaviour
     }
     private void Awake()
     {
+        SetSpriteConsumable(GetSpriteConsumable(consumable));
         if (consumable.Equals(Consumable.TypeConsumable.Money))
         {
             GetComponent<SpriteRenderer>().enabled = false;
@@ -46,6 +48,23 @@ public class ConsumableController : MonoBehaviour
             spawnTime = Time.time;
             speed = 10f;
         }
+    }
+    private void SetSpriteConsumable(Sprite sprite)
+    {
+        GetComponent<SpriteRenderer>().sprite = sprite;
+    }
+    public Sprite GetSpriteConsumable(Consumable.TypeConsumable c)
+    {
+        switch (c)
+        {
+            case Consumable.TypeConsumable.Datil:
+                return spriteDatil;
+            case Consumable.TypeConsumable.Pera:
+                return spritePera;
+            case Consumable.TypeConsumable.Hierbabuena:
+                return spriteHierbabuena;
+        }
+        return null;
     }
     private void Start()
     {
